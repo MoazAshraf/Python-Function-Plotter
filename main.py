a38775e7492e9c17719477727f7614ed641e8b25
Moaz Ashraf <mouadhashraf@gmail.com>
2021-04-17 22:03:35 +0200

    Create app and show main widget placeholder

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 ## The entry point of the program that runs to start the application
+
+import sys
+from PySide2 import QtCore
+from PySide2.QtWidgets import QApplication, QLabel
+
 if __name__ == "__main__":
-    pass
+    # create the Qt application
+    app = QApplication([])
+
+    # create and show the widget placeholder
+    label = QLabel("Main Widget Placeholder", alignment=QtCore.Qt.AlignCenter)
+    label.resize(600, 400)
+    label.setWindowTitle("Function Plotter")
+    label.show()
+
+    # run the main Qt loop
+    sys.exit(app.exec_())
